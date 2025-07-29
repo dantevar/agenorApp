60ccd22df150e1e53e0ae1782b4957b0a13c4326
Gabrijela <garijela.klepec1234@gmail.com>
2025-07-29 17:29:37 +0200

    g

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,5 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.render('index', { title: 'Početna stranica' });
-});
-
-
+//
 module.exports = router;
--- routes/ocevidnici.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const path = require('path');
-const router = express.Router();
-
-router.get('/ocevidnik_01', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/ocevidnici/01.html'));
-});
-
-module.exports = router;
