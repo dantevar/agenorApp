ac7b69236629055f4eaa4c9212541561ea6b7296
dantevar <marko.dukin@hotmail.com>
2025-07-30 11:46:54 +0200

    x

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const path = require("path");
 const bodyParser = require("body-parser");
 const { PDFDocument, StandardFonts } = require("pdf-lib");
 const fs = require("fs");
+const { route } = require("./routes");
 
 const pool = new Pool({
   user: "postgres",
@@ -16,58 +17,21 @@ const pool = new Pool({
   port: 5432,
 });
 
+
 //express app
 const app = express();
 const port = 3001;
 
-app.use(express.static(path.join(__dirname, "public")));
+const indexRouter = require('./routes/index');
 
-app.use(cors()); // Enable all origins
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
 
-app.post("/submit", async (req, res) => {
-  const { OBJEKT, DATUM, PODACI, FIRMA, UMJERAVANJE, SLJEDECE, OSOBA, VALUE } =
-    req.body;
+app.use('/', indexRouter);
 
-  try {
-    const existingPdfBytes = fs.readFileSync(
-      path.join(__dirname, "public", "pdfs", "1.1pdf.pdf")
-    );
-    const pdfDoc = await PDFDocument.load(existingPdfBytes);
-    const pages = pdfDoc.getPages();
-    const firstPage = pages[0];
-    const { width, height } = firstPage.getSize();
-
-    const font = await pdfDoc.embedFont(StandardFonts.Helvetica);
-
-    firstPage.drawText(OBJEKT, { x: 50, y: height - 50, size: 12, font });
-    firstPage.drawText(DATUM, { x: 50, y: height - 70, size: 12, font });
-    firstPage.drawText(PODACI, { x: 50, y: height - 90, size: 12, font });
-    firstPage.drawText(FIRMA, { x: 50, y: height - 110, size: 12, font });
-    firstPage.drawText(UMJERAVANJE, { x: 50, y: height - 130, size: 12, font });
-    firstPage.drawText(SLJEDECE, { x: 50, y: height - 150, size: 12, font });
-    firstPage.drawText(OSOBA, { x: 50, y: height - 170, size: 12, font });
-    firstPage.drawText(VALUE.toString(), {
-      x: 50,
-      y: height - 190,
-      size: 12,
-      font,
-    });
-
-    const pdfBytes = await pdfDoc.save();
-    const outputPath = path.join(__dirname, "public", "pdfs", "filled-pdf.pdf");
-    fs.writeFileSync(outputPath, pdfBytes);
-
-    res.redirect("/pdfs/filled-pdf.pdf");
-  } catch (error) {
-    console.error("Greška:", error);
-    res.status(500).send("Dogodila se greška.");
-  }
-});
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.render('index', { title: 'Početna stranica' });
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 
