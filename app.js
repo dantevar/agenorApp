f8a76e45896e083ff8b4bb181c115d42aff579c8
dantevar <marko.dukin@hotmail.com>
2025-07-30 16:11:53 +0200

    baze starting

--- app.js
+++ app.js
@@ -2,21 +2,10 @@
 const express = require("express");
 const cors = require("cors");
 
-const { Pool } = require("pg");
 const path = require("path");
 const bodyParser = require("body-parser");
 const { PDFDocument, StandardFonts } = require("pdf-lib");
 const fs = require("fs");
-const { route } = require("./routes");
-
-const pool = new Pool({
-  user: "postgres",
-  host: "192.168.5.95",
-  database: "agenor",
-  password: "admin",
-  port: 5432,
-});
-
 
 //express app
 const app = express();
@@ -24,7 +13,7 @@ const port = 3001;
 
 const indexRouter = require('./routes/index');
 
-
+app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
---
+++ db/index.js
@@ -0,0 +1,15 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "postgres",
+  host: "192.168.5.95",
+  database: "agenor",
+  password: "admin",
+  port: 5432,
+});
+
+module.exports = pool;
+
+module.exports = {
+    query: (text, params) => pool.query(text, params),
+}
