e28f71bec1d9cb3f146a252e9d2302794d7214a7
dantevar <marko.dukin@hotmail.com>
2025-08-06 10:12:48 +0200

    rute

--- public/web-components/izbornik.js
+++ public/web-components/izbornik.js
@@ -7,21 +7,22 @@ class NavMenu extends HTMLElement {
       </style>
       <ul class="sidebar-nav">
         <h1>IZBORNIK</h1>
-        <li><a href="../templates/01_umj_mjeraca.html">UMJERAVANJE MJERAČA ZA RUČNO MJERENJE</a></li>
-        <li><a href="../templates/02_god_ispiranje.html">GODIŠNJE ISPIRANJE PROTOČNOG SUSTAVA BAZENA</a></li>
-        <li><a href="../templates/03_dodana_voda.html">DODANA VODA</a></li>
-        <li><a href="../templates/04_broj_posjetitelja.html">BROJ POSJETITELJA</a></li>
-        <li><a href="../templates/05_ukl_nedostataka.html">UKLANJANJE NEDOSTATAKA</a></li>
-        <li><a href="../templates/06_rez_lab_analiza.html">REZULTATI LABORATORIJSKIH ANALIZA</a></li>
-        <li><a href="../templates/07_ispir_filtera.html">IZMJERENI POKAZATELJI - SLATKA VODA</a></li>
-        <li><a href="../templates/08_slatka_voda.html">IZMJERENI POKAZATELJI - MORSKA VODA</a></li>
-        <li><a href="../templates/09_morska_voda.html">ISPIRANJE FILTARA BAZENSKIH KUPALIŠTA</a></li>
-        <li><a href="../templates/10_hidromasazni_bazeni.html">IZMJERENI POKAZATELJI - HIDROMASAŽNI BAZENI</a></li>
-        <li><a href="../templates/11_plan_ciscenja.html">PLAN ČIŠĆENJA, PRANJA I DEZINFEKCIJE BAZENSKOG PROSTORA</a></li>
-        <li><a href="../templates/12_evidencija_ciscenja.html">EVIDENCIJA ČIŠĆENJA, PRANJA I DEZINFEKCIJE BAZENSKOG PROSTORA</a></li>
+        <li><a href="/menu/umj_mjeraca">UMJERAVANJE MJERAČA ZA RUČNO MJERENJE</a></li>
+        <li><a href="/menu/god_ispiranje">GODIŠNJE ISPIRANJE PROTOČNOG SUSTAVA BAZENA</a></li>
+        <li><a href="/menu/dodana_voda">DODANA VODA</a></li>
+        <li><a href="/menu/broj_posjetitelja">BROJ POSJETITELJA</a></li>
+        <li><a href="/menu/ukl_nedostataka">UKLANJANJE NEDOSTATAKA</a></li>
+        <li><a href="/menu/rez_lab_analiza">REZULTATI LABORATORIJSKIH ANALIZA</a></li>
+        <li><a href="/menu/ispir_filtera">IZMJERENI POKAZATELJI - SLATKA VODA</a></li>
+        <li><a href="/menu/slatka_voda">IZMJERENI POKAZATELJI - MORSKA VODA</a></li>
+        <li><a href="/menu/morska_voda">ISPIRANJE FILTARA BAZENSKIH KUPALIŠTA</a></li>
+        <li><a href="/menu/hidromasazni_bazeni">IZMJERENI POKAZATELJI - HIDROMASAŽNI BAZENI</a></li>
+        <li><a href="/menu/plan_ciscenja">PLAN ČIŠĆENJA, PRANJA I DEZINFEKCIJE BAZENSKOG PROSTORA</a></li>
+        <li><a href="/menu/evidencija_ciscenja">EVIDENCIJA ČIŠĆENJA, PRANJA I DEZINFEKCIJE BAZENSKOG PROSTORA</a></li>
       </ul>
     `;
   }
 }
 
 customElements.define("nav-menu", NavMenu);
+
