c45767f1683d8ee1d0e4bbfbdd375be32b4e9f51
an33rol <lorenaivanisevic@proton.me>
2025-08-06 12:18:28 +0200

    change izbornik style

--- public/web-components/izbornik.js
+++ public/web-components/izbornik.js
@@ -10,18 +10,19 @@ class NavMenu extends HTMLElement {
       </style>
       <ul class="sidebar-nav">
         <h1>IZBORNIK</h1>
-        <li><a href="/menu/umj_mjeraca">UMJERAVANJE MJERAČA ZA RUČNO MJERENJE</a></li>
-        <li><a href="/menu/god_ispiranje">GODIŠNJE ISPIRANJE PROTOČNOG SUSTAVA BAZENA</a></li>
-        <li><a href="/menu/dodana_voda">DODANA VODA</a></li>
-        <li><a href="/menu/broj_posjetitelja">BROJ POSJETITELJA</a></li>
-        <li><a href="/menu/ukl_nedostataka">UKLANJANJE NEDOSTATAKA</a></li>
-        <li><a href="/menu/rez_lab_analiza">REZULTATI LABORATORIJSKIH ANALIZA</a></li>
-        <li><a href="/menu/ispir_filtera">IZMJERENI POKAZATELJI - SLATKA VODA</a></li>
-        <li><a href="/menu/slatka_voda">IZMJERENI POKAZATELJI - MORSKA VODA</a></li>
-        <li><a href="/menu/morska_voda">ISPIRANJE FILTARA BAZENSKIH KUPALIŠTA</a></li>
-        <li><a href="/menu/hidromasazni_bazeni">IZMJERENI POKAZATELJI - HIDROMASAŽNI BAZENI</a></li>
-        <li><a href="/menu/plan_ciscenja">PLAN ČIŠĆENJA, PRANJA I DEZINFEKCIJE BAZENSKOG PROSTORA</a></li>
-        <li><a href="/menu/evidencija_ciscenja">EVIDENCIJA ČIŠĆENJA, PRANJA I DEZINFEKCIJE BAZENSKOG PROSTORA</a></li>
+    <li><a href="/menu/umj_mjeraca">Umjeravanje mjerača za ručno mjerenje</a></li>
+<li><a href="/menu/god_ispiranje">Godišnje ispiranje protočnog sustava bazena</a></li>
+<li><a href="/menu/dodana_voda">Dodana voda</a></li>
+<li><a href="/menu/broj_posjetitelja">Broj posjetitelja</a></li>
+<li><a href="/menu/ukl_nedostataka">Uklanjanje nedostataka</a></li>
+<li><a href="/menu/rez_lab_analiza">Rezultati laboratorijskih analiza</a></li>
+<li><a href="/menu/ispir_filtera">Izmjereni pokazatelji - slatka voda</a></li>
+<li><a href="/menu/slatka_voda">Izmjereni pokazatelji - morska voda</a></li>
+<li><a href="/menu/morska_voda">Ispiranje filtara bazenskih kupališta</a></li>
+<li><a href="/menu/hidromasazni_bazeni">Izmjereni pokazatelji - hidromasažni bazeni</a></li>
+<li><a href="/menu/plan_ciscenja">Plan čišćenja, pranja i dezinfekcije bazenskog prostora</a></li>
+<li><a href="/menu/evidencija_ciscenja">Evidencija čišćenja, pranja i dezinfekcije bazenskog prostora</a></li>
+
       </ul>
     `;
   }
